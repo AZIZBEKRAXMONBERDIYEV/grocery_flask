7f60fe3de70b334bcf405359ce07960c86692026
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-04-29 19:07:03 +0500

    ass

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , request
 from db import GroceryDB
 
 
@@ -10,35 +10,43 @@ db = GroceryDB()
 @app.route('/grocery')
 def all_grocery():
     """Get all grocery"""
-    pass
+    conttext=db.all()
+    return conttext
+
 
 
 # view add grocery
 @app.route('/grocery/add', methods=['POST'])
 def add_grocery():
     """Add a grocery"""
-    pass
+    conttext=request.get_json()
+    db.add(conttext)
+    return conttext
 
 
 # view all grocery by type
 @app.route('/grocery/type/<type>')
 def all_grocery_by_type(type):
     """Get all grocery by type"""
-    pass
+    conttext=db.get_by_type(type=type)
+    return conttext
+            
 
 
 # view all grocery by name
 @app.route('/grocery/name/<name>')
 def all_grocery_by_name(name):
     """Get all grocery by name"""
-    pass
+    conttext=db.get_by_name(name=name)
+    return conttext
 
 
 # view all grocery by price
 @app.route('/grocery/price/<float:price>')
 def all_grocery_by_price(price):
     """Get all grocery by price"""
-    pass
+    conttext=db.get_by_price(price=price)
+    return conttext
 
 
 
